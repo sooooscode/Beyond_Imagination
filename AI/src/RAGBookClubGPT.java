1d41ce4d3726a9ebb781370c82af9c7e121e9d1f
Minji Bang <147597686+BANG-mj@users.noreply.github.com>
2024-11-30 23:18:42 +0900

    Update RAGBookClubGPT.java

--- AI/src/RAGBookClubGPT.java
+++ AI/src/RAGBookClubGPT.java
@@ -1,23 +1,32 @@
 public class RAGBookClubGPT {
-    // 필요한 변수 및 생성자 정의
+    private PDFLoader pdfLoader;
+    private TextProcessor textProcessor;
+    private ResponseFilter responseFilter;
+    private DiscussionManager discussionManager;
+
+    // 생성자
+    public RAGBookClubGPT(String pdfPath) {
+        this.pdfLoader = new PDFLoader(pdfPath);
+        this.textProcessor = new TextProcessor();
+        this.responseFilter = new ResponseFilter();
+        this.discussionManager = new DiscussionManager();
+    }
 
     public void generateTopics(List<String> userResponses) {
-        // 발제문 생성 로직
+        String combinedText = textProcessor.combineText(userResponses);
+        List<String> topics = textProcessor.prepareContextForTopics(Collections.singletonList(combinedText));
+        discussionManager.createTopics(topics);
     }
 
     public List<String> collectResponses(String topic) {
-        // 참가자 의견 수집 로직
+        return discussionManager.collectUserResponses(topic);
     }
 
-    public List<String> filterResponses(List<String> responses) {
-        // 필터링 로직
-    }
-
-    public void freeDiscussion() {
-        // 자유 토론 시간 로직
+    public void startFreeDiscussion() {
+        discussionManager.startFreeDiscussion();
     }
 
     public void summarizeMeeting(List<String> topics, List<List<String>> allResponses) {
-        // 모임 요약 로직
+        discussionManager.summarizeMeeting(topics, allResponses);
     }
 }
