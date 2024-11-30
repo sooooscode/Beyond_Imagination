2029ab4847968bffe9daa016a2c0ecea28a3011d
Minji Bang <147597686+BANG-mj@users.noreply.github.com>
2024-11-30 23:10:48 +0900

    Create RAGBookClubGPT.java

---
+++ AI/src/RAGBookClubGPT.java
@@ -0,0 +1,23 @@
+public class RAGBookClubGPT {
+    // 필요한 변수 및 생성자 정의
+
+    public void generateTopics(List<String> userResponses) {
+        // 발제문 생성 로직
+    }
+
+    public List<String> collectResponses(String topic) {
+        // 참가자 의견 수집 로직
+    }
+
+    public List<String> filterResponses(List<String> responses) {
+        // 필터링 로직
+    }
+
+    public void freeDiscussion() {
+        // 자유 토론 시간 로직
+    }
+
+    public void summarizeMeeting(List<String> topics, List<List<String>> allResponses) {
+        // 모임 요약 로직
+    }
+}
