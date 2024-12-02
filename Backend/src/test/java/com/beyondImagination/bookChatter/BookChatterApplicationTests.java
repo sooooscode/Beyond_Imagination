fb3cc0f58f309dcba26c5cca91e45c96f2b01b08
soo <sooooscode@gmail.com>
2024-12-03 01:47:00 +0900

    새로운 spring boot 프로젝트 생성

---
+++ Backend/src/test/java/com/beyondImagination/bookChatter/BookChatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondImagination.bookChatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookChatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
