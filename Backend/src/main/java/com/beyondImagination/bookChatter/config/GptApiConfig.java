16b31385f4ab000863c9ab24dd5226313f0d8de6
soo <sooooscode@gmail.com>
2024-12-13 03:05:57 +0900

    gpt api 환경번수 재재설정

--- Backend/src/main/java/com/beyondImagination/bookChatter/config/GptApiConfig.java
+++ Backend/src/main/java/com/beyondImagination/bookChatter/config/GptApiConfig.java
@@ -1,5 +1,6 @@
 package com.beyondImagination.bookChatter.config;
 
+<<<<<<< HEAD
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +16,18 @@ public class GptApiConfig {
 
   public void setKey(String key) {
     this.key = key;
+=======
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GptApiConfig {
+
+  @Value("${gpt.api.key}") // application.properties에서 gpt.api.key 값을 가져옴
+  private String apiKey;
+
+  public String getApiKey() {
+    return apiKey;
+>>>>>>> 38287ecc (최종 오류 해결 및 빌드 성공: 중복된 org.springframework:spring-context를 삭제함, gpt.api key를 추가함)
   }
 }
